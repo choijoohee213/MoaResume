63b38b190c2d47c4cd76bd96c24dd20b5a86758f
choijoohee213 <choijoohee@naver.com>
2025-10-08 23:49:46 +0900

    chore : reviewdog comment 테스트를 위한 unused코드 추가

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QPushButton>
 
 int main(int argc, char *argv[]) {
+    int unused; // test for reviewdog
     QApplication a(argc, argv);
     QPushButton button("Hello MoaResume!", nullptr);
     button.resize(200, 100);
