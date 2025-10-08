c0431cfd75df8ba1468354aab8fa26d1286a9feb
choijoohee213 <choijoohee@naver.com>
2025-10-09 02:39:05 +0900

    test : test for cppCheck(github-actions)

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <QPushButton>
 
 int main(int argc, char *argv[]) {
-    int unused; // test for reviewdog
     QApplication a(argc, argv);
     QPushButton button("Hello MoaResume!", nullptr);
     button.resize(200, 100);
     button.show();
+    int result;
     return QApplication::exec();
 }
