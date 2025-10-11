e568e307efcb7fa8dfbf1f6f1bcd8c1f7fd9a520
choijoohee213 <choijoohee@naver.com>
2025-10-11 18:18:47 +0900

    feature : HomeWidget.h/cpp 추가(홈화면 위젯)

---
+++ pkg/home/widget/HomeWidget.h
@@ -0,0 +1,34 @@
+#ifndef HOMEWIDGET_H
+#define HOMEWIDGET_H
+
+#include <QWidget>
+
+class QPushButton;
+class QLabel;
+class QVBoxLayout;
+
+class HomeWidget : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit HomeWidget(QWidget *parent = nullptr);
+
+    ~HomeWidget() = default;
+
+signals:
+    void navigateToWorkspace();
+
+private slots:
+    void onWorkSpaceClicked();
+
+private:
+    void setupUi();
+
+    void connectSignals();
+
+    QLabel *titleLabel;
+    QPushButton *appManagementButton;
+    QVBoxLayout *mainLayout;
+};
+
+#endif
