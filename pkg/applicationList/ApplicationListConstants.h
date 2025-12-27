72f0ccece7417d19d5d410b209166b4e9afffd18
choijoohee213 <choijoohee@naver.com>
2025-12-27 18:27:57 +0900

    feat: ApplicationList Json 저장/로드 구현

---
+++ pkg/applicationList/ApplicationListConstants.h
@@ -0,0 +1,8 @@
+#ifndef APPLICATIONLISTCONSTANTS_H
+#define APPLICATIONLISTCONSTANTS_H
+
+namespace ApplicationListConstants {
+    constexpr const char *APPLICATION_LIST_JSON_FILE_NAME = "applicationList.json";
+};
+
+#endif //APPLICATIONLISTCONSTANTS_H
--- pkg/applicationList/service/ApplicationListService.cpp
+++ pkg/applicationList/service/ApplicationListService.cpp
@@ -1,8 +1,15 @@
 #include "ApplicationListService.h"
+#include "../../../common/util/JsonFileUtils.h"
+#include "../ApplicationListConstants.h"
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QDebug>
 
 ApplicationListService::ApplicationListService(QObject *parent)
     : QObject(parent)
       , mNextId(0) {
+    load();
 }
 
 ApplicationListService &ApplicationListService::getInstance() {
@@ -66,10 +73,47 @@ bool ApplicationListService::deleteApplication(int id) {
 }
 
 bool ApplicationListService::save() {
-    return true;
+    QJsonArray jsonArray;
+    for (const Application &app: mApplications) {
+        jsonArray.append(app.toJson());
+    }
+
+    QJsonObject rootObject;
+    rootObject["applications"] = jsonArray;
+    rootObject["nextId"] = mNextId;
+
+    QJsonDocument doc(rootObject);
+    QString filePath = JsonFileUtils::getDataFilePath(ApplicationListConstants::APPLICATION_LIST_JSON_FILE_NAME);
+
+    return JsonFileUtils::saveJsonToFile(filePath, doc);
 }
 
 bool ApplicationListService::load() {
+    QString filePath = JsonFileUtils::getDataFilePath(ApplicationListConstants::APPLICATION_LIST_JSON_FILE_NAME);
+
+    if (!JsonFileUtils::fileExists(filePath)) {
+        mApplications.clear();
+        mNextId = 0;
+        return true;
+    }
+
+    QJsonDocument doc = JsonFileUtils::loadJsonFromFile(filePath);
+    if (doc.isNull() || !doc.isObject()) {
+        qWarning() << "Invalid JSON format";
+        return false;
+    }
+
+    QJsonObject rootObject = doc.object();
+    QJsonArray jsonArray = rootObject["applications"].toArray();
+    mNextId = rootObject["nextId"].toInt(0);
+
+    mApplications.clear();
+    for (const QJsonValue &value: jsonArray) {
+        if (value.isObject()) {
+            mApplications.append(Application::fromJson(value.toObject()));
+        }
+    }
+
     return true;
 }
 
