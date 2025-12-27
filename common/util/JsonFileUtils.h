32e60bfea6503173c119d3c0f9bb7e730e4cc244
choijoohee213 <choijoohee@naver.com>
2025-12-27 18:27:23 +0900

    feat: Json file 저장/로드 등 관리를 위한 JsonFileUtils 구현

---
+++ common/util/JsonFileUtils.h
@@ -0,0 +1,20 @@
+#ifndef JSONFILEUTILS_H
+#define JSONFILEUTILS_H
+
+#include <QString>
+#include <QJsonDocument>
+
+class JsonFileUtils {
+public:
+    static QString getDataDirectory();
+
+    static QString getDataFilePath(const QString& filename);
+
+    static bool saveJsonToFile(const QString& filePath, const QJsonDocument& doc);
+
+    static QJsonDocument loadJsonFromFile(const QString& filePath);
+
+    static bool fileExists(const QString& filePath);
+};
+
+#endif // JSONFILEUTILS_H
